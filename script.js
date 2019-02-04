045e9b3038568df9d3a3a145bbe1ca9e51ee0430
israelsixto <israelsixtosanchez@gmail.com>
2019-02-04 12:30:29 -0800

    add stuff

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-$('.nav li').click(function(){
-    $(this).addClass('active').siblings().removeClass('active');
-})
+$('.uiButton').unbind('click').click(function() {
+    $(this).attr('class', 'uiButton-active');
+  });
