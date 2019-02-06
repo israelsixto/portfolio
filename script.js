bfca534da00233f278bef204289768f3c09f041f
israelsixto <israelsixtosanchez@gmail.com>
2019-02-06 07:35:53 -0800

    finish nav

---
+++ script.js
@@ -0,0 +1,13 @@
+function chmg(Number) {
+    document.getElementById('slider').style.marginLeft = Number + '%';
+};
+
+var base = 0;
+
+var work = 0;
+var about = 33.33;
+var resume = 66.66;
+
+
+//const color = element.style.color
+//console.log(color) // red
